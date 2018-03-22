4654e363fbe79ae2a35c82d2f27313beda6e110a
OpportunV <Vadya.95@mail.ru>
2018-03-22 06:20:54 +0300

    M

---
+++ Rocket!/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class AudioManager : MonoBehaviour {
+
+    #region Singleton and DontDestroy
+    public static AudioManager instance;
+	void Awake() {
+        if (instance == null) {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Debug.LogWarning("more than one instance of AudioManager");
+            Destroy(gameObject);
+        }
+	}
+    #endregion
+
+    void Update() {
+
+    }
+}
